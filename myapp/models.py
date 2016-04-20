bb7a9ea4c47afb187279d882cbc90d90d2969f10
Andy <andyg3590@gmail.com>
2016-04-19 21:21:19 -0400

    Starting the model

--- myapp/models.py
+++ myapp/models.py
@@ -11,7 +11,18 @@ class Class(models.Model):
     endTime = models.TimeField()
     startDate = models.DateField()
     endDate = models.DateField()
-    
+    lateThreshold = models.TimeField()
+    absentThreshold = models.TimeField()
+    locationFlag = models.BooleanField(blank=True, default=False)
+    latitude = models.DecimalField(null=True, blank=True, max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(null=True, blank=True,max_digits=9, decimal_places=6)
+    codeFlag = models.BooleanField(blank=True, default=False)
+    code = models.CharField(null=True, blank=True,max_length=5)
+    codeExpirationDate = models.DateTimeField(null=True, blank=True)
+
+
+
+
     def __str__(self):
         return self.classId
 
@@ -19,19 +30,19 @@ class Attendance(models.Model):
     classId = models.ForeignKey(Class, on_delete=models.CASCADE)
     studentName = models.CharField(max_length=200)
     date = models.DateField()
-    ATTEMPT = 'AT'
-    PRESENT = 'PR'
-    LATE = 'LT'
-    ABSENT = 'AB'
+    ATTEMPT = 'Attempt'
+    PRESENT = 'Present'
+    LATE = 'Late'
+    ABSENT = 'Absent'
     MARK_CHOICES = (
         (ATTEMPT, 'Attempt'),
         (PRESENT, 'Present'),
         (LATE, 'Late'),
         (ABSENT, 'Absent'),
     )
-    mark = models.CharField(max_length=2,
+    mark = models.CharField(max_length=10,
                                       choices=MARK_CHOICES,
                                       default=ATTEMPT)
-    
+
     def __str__(self):
         return self.studentName
