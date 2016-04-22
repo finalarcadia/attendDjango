377445e930546b4a1f38ce2708cc1eb0cce0d36e
Andy <andyg3590@gmail.com>
2016-04-22 19:21:04 -0400

    adding whole model, needs revision

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Class(models.Model):
+    ClassPK = models.IntegerField(primary_key=True)
     classId = models.CharField(max_length=30)
     name = models.CharField(max_length=100)
     startTime = models.TimeField()
@@ -19,17 +20,27 @@ class Class(models.Model):
     codeFlag = models.BooleanField(blank=True, default=False)
     code = models.CharField(null=True, blank=True,max_length=5)
     codeExpirationDate = models.DateTimeField(null=True, blank=True)
-
-
-
+    universityKey = models.ForeignKey(UniversityDim, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.classId
 
 class Attendance(models.Model):
-    classId = models.ForeignKey(Class, on_delete=models.CASCADE)
-    studentName = models.CharField(max_length=200)
+    attendancePK = models.IntegerField(primary_key=True)
+
+    # below are the joins
+    classkey = models.ForeignKey(Class, on_delete=CASCADE)
+    userIdKey = models.ForeignKey(auth_user, on_delete=CASCADE)
+    #end of joins
+
+    classId = models.IntegerField() #get from class join
+    className = models.CharField() #get from class join
+    studentFirstName = models.CharField() #get from user_id
+    studentLastName = models.Charfield() #get from user_id
+
     date = models.DateField()
+    time = models.TimeField()
+
     ATTEMPT = 'Attempt'
     PRESENT = 'Present'
     LATE = 'Late'
@@ -46,3 +57,43 @@ class Attendance(models.Model):
 
     def __str__(self):
         return self.studentName
+
+class UniversityDim(models.Model):
+    UniversityDimPK = models.IntegerField(primary_key=True)
+    universityCode = models.IntegerField()
+    universityName = models.Charfield()
+
+class UserDetailDim(models.Model):
+    UserDetailDimPK = models.IntegerField(primary_key=True)
+    schoolId = models.IntegerField()
+    #below are the joins
+    universityKey = models.ForeignKey(UniversityDim, on_delete=models.CASCADE)
+    userIdKey = models.ForeignKey(auth_user, on_delete=CASCADE)
+    #end of joins
+    user_id = models.IntegerField()
+    universityCode = models.IntegerField()
+
+class ClassRequest(models.Model):
+    classRequestPK = models.IntegerField(primary_key=True)
+    #below are the joins
+    userIdKey = models.ForeignKey(auth_user, on_delete=CASCADE)
+    schoolIdKey = ForeignKey(UserDetailDim, on_delete=CASCADE)
+    classIdKey = models.ForeignKey(Class, on_delete=models.CASCADE)
+    #end of joins
+    className = models.CharField()
+    universityName = models.Charfield() #get from school_id join
+    userType = models.BooleanField(default=False) #get from user_id join 1 for professor, 0 for student
+
+
+class ClassRoster (models.Model):
+    classRosterPK = models.IntegerField(primary_key=True)
+
+    #below are the joins
+    classIdKey = ForeignKey(Class, on_delete=True)
+    userIdKey = models.ForeignKey(auth_user, on_delete=CASCADE)
+    #end of joins
+
+    className = models.Charfield() #get from class join
+    studentName = models.Charfield()  #get from user_id join
+    professorName = models.Charfield()  #get from user_id join
+    adminName = models.Charfield() #get from user_id join
